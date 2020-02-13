533df18669f1e74fd5388300abc2c776fd02cee9
Katie <kmurley@gmail.com>
2020-02-13 14:09:43 +0100

    .

--- run_event.sh
+++ run_event.sh
@@ -6,6 +6,6 @@ use root
 
 RUNNR=$(printf %06d $SLURM_ARRAY_TASK_ID)
 
-cd /vol/astro3/lofar/lora/LORA_software_V2
+cd /vol/astro3/lofar/lora/LORA_software_V2/LORA_software_V2
 
 python LORA_by_event.py -i $RUNNR
