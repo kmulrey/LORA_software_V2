a75716805c9c80d566d15be0c32a57d2ab21244c
Katie <kmurley@gmail.com>
2020-02-23 22:21:37 +0100

    .

--- event.py
+++ event.py
@@ -281,7 +281,7 @@ def cal_event_timestamp(detectors,lasa):
                 trigg_time=thresh_use[args[0]]
             except: #this is in here becasue in a few cases the length of thresh_use was less than trigg. condition
                 trigg_time=np.sort(thresh_use)[len(thresh_use)-1]
-            #print 'V2: ',thresh_times,trigg_time
+            print 'V2: ',thresh_times,trigg_time
         
         
 
